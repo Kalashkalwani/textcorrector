bf0813439e1f34915cc480ea6f2c2d092c38592b
kalashkalwani <kalashkalwani2000@gmail.com>
2021-01-25 12:03:44 +0530

    additional files and changes for Heroku

--- django_web/settings.py
+++ django_web/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 INSTALLED_APPS = [
 'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
